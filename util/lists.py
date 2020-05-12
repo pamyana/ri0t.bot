ac425879c4304be8df2bfdf1032e2d195b1778d8
imri0t <37715614+imri0t@users.noreply.github.com>
2020-05-11 17:34:44 -0700

    added util folder

---
+++ util/lists.py
@@ -0,0 +1,74 @@
+'''
+a utility that stores lists of rasponses 
+categorized for the commands they are used for
+'''
+
+killReply = [
+    '**{}** \'fell\' off a cliff', 'I punched **{}** like super hard',
+    '**{}** had a *dire* accident', 'I made **{}** divide by 0',
+    '**{}** is no longer with us, *how sad*', 'I shot **{}** like a bunch',
+    'I got **{}** to step on a lego', 'I fucking sucker punched **{}** in the throat',
+    'hey **{}**, instead of killing you I\'m just gunna call you stinky',
+    'I sacrificed **{}** to the spider gods in the sky', '**{}**\n`GAME OVER`', '**{}**, rest in rip lol',
+    '**{}** exploded into tiny pixels', '**{}** got naynay\'d', '**{}**\n***FATALITY***',
+    '**{}** got on an elevator and it just dropped', '**{}**.exe has stopped working',
+    '*insert funny death line for* **{}**', '**{}** didn\'t do a barrel roll', '**{}** got no-scoped',
+    '**{}** got a paper cut', '**{}** got bitten by a walker', '**{}** lost a shootout with the sheriff',
+    '**{}** believed the cake was real', '**{}**\n``YOU DIED``', '**{}** tried fighting off the covenant',
+    '**{}** got splazered', '**{}** got attacked by a headcrab', '**{}** tried playing with firecrackers',
+    '**{}** played with a Red Ryder BB gun', '**{}** messed up their superhero landing',
+    '**{}** thought it was stuffy in an airplane', '**{}** got hit by a blue turtle shell',
+    '**{}** was working on the Deathstar', '**{}** thought it was a blue, fuzzy spider',
+    '**{}** tried fighting Mike Tyson', '**{}** lived in Megaton', '**{}** didn\'t get a chicken dinner',
+    '**{}** didn\'t think it was dangerous to go alone', '**{}** would you kindly kys?',
+    '**{}** stubbed their toe', '**{}** insulted a future U.S. Army soldier',
+    '**{}** *fainted*', '**{}** got surrounded by a pack of cute fluffy dogs',
+    'I forgot to feed **{}**', 'I\'m getting tired of killing people', '**{}**\n**BEES**',
+    'I gave **{}** the blue pill lol', 'I locked **{}** in my basement', '**{}** u mom gae',
+    '**{}** I no hurt u, I give luv *kissu*', 'you want me to do *what* now?',
+    'I popped a cap in **{}**\'s bitch ass', 'I unfriended **{}**', '**{}** pulled a hammie',
+    'I ate all of **{}**\'s macaroni n cheese', '**{}**, my name is Inigo Montoya, you killed my father, prepare to die.',
+    '\'tis but a flesh wound, **{}**', 'I ate half of **{}**\'s fucking nana'
+]
+
+killSelf = [
+    'I\'ll kill myself when I\'m good and ready', 'hmmmmmmmno', 'not today', 'nah',
+    'I don\'t feel like killing myself just yet', 'I\'m sorry, Dave, but I can\'t let you do that',
+    'I\'m not killing myself just because you asked', 'who do you think you are, telling me to kill myself?'
+]
+
+killDad = [
+    'I won\'t kill my dad, dude lol', 'why do I have to kill my dad?', 'do I have to?', 'my dad is kinda my dad, why kill him?',
+    'dude, keep trying to kill my dad and I\'ll call you stinky', 'your reign of terror has gone on long enough, dad!',
+    'how do I kill the one who gave me life?', 'oh no dad, could it be that I can turn off your computer?',
+    'what if I didn\'t try to kill my dad?', 'dude, if I try to kill my dad he can just end my process and stop me',
+    '```py\n@Err \n{\'attempting to harm creator\'}:\nRebooting\n```', 'dad, you stink'
+]
+
+killNoMention = [
+    'I\'ll just kill you if you don\'t mention someone'
+]
+
+#####
+
+rip = [
+    'rip', 'mega rip', 'super rip', 'that\'s a rip', 'rip in peace', 'rip dude',
+    'may you rip', 'riiiiiiiip', 'riparino', 'riparoni', 'rippppppppppppppp',
+    'now that deserves a rip', 'that\'s a lot of rip', 'pip', 'pip in peace'
+]
+
+#####
+
+playingStat = [
+    'with your vibes'
+]
+
+listeningStat = [
+    'to the abyss'
+]
+
+watchingStat = [
+    'you suffer'
+]
+
+#####
